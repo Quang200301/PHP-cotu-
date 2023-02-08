809c48aa15133a7e1e4ea4de6d3d7db6c6208c60
quang200301 <quang.a24@student.passerellesnumeriques.org>
2023-02-08 18:19:02 +0700

    file

---
+++ pnhan.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>BẢNG CỬU CHƯƠNG</title>
+</head>
+<body>
+    <?phP
+        $a=2;
+        $b=9;
+        for($i=2;$i<=9;$i++){
+            echo"BẢNG CỬU CHƯƠNG $i";
+            echo "<br>";
+            for($j=1;$j<=9;$j++){
+                echo"$i*$j=".$i*$j;
+
+                echo "<br>";
+            }
+            echo "<br>";
+        }
+
+    ?>
+</body>
+</html>
