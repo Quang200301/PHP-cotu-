ccd15f985625b61c9cb8678ea24c696256a968d9
quang200301 <quang.a24@student.passerellesnumeriques.org>
2023-02-10 08:09:23 +0700

    newbt

--- pnhan.php
+++ pnhan.php
@@ -5,9 +5,8 @@
 </head>
 <body>
     <?phP
-        $a=2;
-        $b=9;
-        for($i=2;$i<=9;$i++){
+        $b=4;
+        for($i=1;$i<=$b;$i++){
             echo"BẢNG CỬU CHƯƠNG $i";
             echo "<br>";
             for($j=1;$j<=9;$j++){
---
+++ ucln.php
@@ -0,0 +1,28 @@
+<?php
+$a=12;$b=30;
+if($a>$b){
+        for($i=1;$i<=$b;$i++){
+                if($a%$i==0 && $b%$i==0){
+                        $c=$i;
+                }
+        }
+        if($a*$b%c==0){
+           $d=$a*$b/$c;
+        }
+}
+else if($a<$b){
+             for($i=1;$i<=$a;$i++){
+                if($a%$i==0 && $b%$i==0){
+            
+                        $c=$i;
+                }
+            }       
+            if($a*$b%$c==0){
+               $d=$a*$b/$c;
+            }
+};
+echo"ước chung lớn nhất của $a và $b là $c <br> bội chung nhỏ nhất của $a và $b là $d";
+?>
+
+
+
