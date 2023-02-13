533b61cc1756945f8bce2b32f3e79e7f9453963c
quang200301 <quang.a24@student.passerellesnumeriques.org>
2023-02-13 07:38:16 +0700

    newbt

---
+++ tongn.php
@@ -0,0 +1,20 @@
+<html>
+    <body>
+        <form action="" method=post>
+            <label for="">input number:</label>
+            <input type="number" name="nb">
+            <input type="submit" value="check">
+        </form>
+        <?php
+        if (isset($_POST['nb'])){
+            $nb1=$_POST['nb'];
+            $sum=0;
+            while($nb1>0){
+                $sum=$sum+($nb1%10);
+                $nb1=$nb1/10;
+            }
+            echo "sum is:$sum";
+        }
+        ?>
+    </body>
+</html>
